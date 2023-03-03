c38eab2a06073c845d3ef5bcc477c246ed17c924
Jeremias <abellojeremias@gmail.com>
2023-03-02 21:22:45 -0300

    se entrega la pre entrega

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import ProductRouter from "./router/product.routes.js";
+import CartRouter from "./router/carts.routes.js";
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use("/products", ProductRouter)
+app.use("/api/cart", CartRouter)
+
+app.listen(PORT, () => {
+    console.log(`Servidor Express Puerto ${PORT}`);
+});
+
